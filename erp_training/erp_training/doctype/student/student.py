3586e512bf54720b6b28ba10925d94cf3dc6603e
Mian Shahid <mianshahid03206893720@gmail.com>
2023-12-28 13:24:14 +0500

    28/12/23

--- erp_training/erp_training/doctype/student/student.py
+++ erp_training/erp_training/doctype/student/student.py
@@ -6,11 +6,25 @@ from frappe.model.document import Document
 
 class Student(Document):
     def validate(self):
-        self.members = []
-        # doc = frappe.get_doc("Nextash Users", self.nextash_user)
-        # frappe.msgprint(f"{doc.designation}")
-        docs = frappe.db.get_list("Nextash Users", filters={'gender': 'Male'}, fields = ['name','designation'], order_by= 'name asc',page_length=2)
-        frappe.msgprint(f"{docs}")
+        # my_value = frappe.db.get_value('Nextash Users', self.nextash_user, ["gender", "designation"], as_dict = 1)
+        # frappe.msgprint(f"{my_value}")
+        
+        # my_value = frappe.db.set_value('Nextash Users', self.nextash_user, {'gender': 'Male', 'designation':'Developer'})
+        # my_value = frappe.db.count('Nextash Users', {"designation": "Developer"})
+        data = frappe.db.sql("""
+    SELECT
+        name,designation
+    FROM `tabNextash Users`
+""", as_dict=1)
+        # my_doc = frappe.get_doc("Nextash Users", "Tania")
+        # my_doc.db_set('designation', "Shahid")
+        frappe.msgprint(f"{data}")
+
+        # self.members = []
+        # # doc = frappe.get_doc("Nextash Users", self.nextash_user)
+        # # frappe.msgprint(f"{doc.designation}")
+        # docs = frappe.db.get_list("Nextash Users", filters={'gender': 'Male'}, fields = ['name','designation'], order_by= 'name asc',page_length=2)
+        # frappe.msgprint(f"{docs}")
         
 		
 		
